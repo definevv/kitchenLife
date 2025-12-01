d9d37170f1a3da5a59bf744d29aef2c82583ad72
definevv <definevv@gmail.com>
2025-12-01 17:12:42 +0900

    second

--- app/src/main/java/com/example/kitchenlife/MainActivity.java
+++ app/src/main/java/com/example/kitchenlife/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.kitchenlife.ui.mealplan.HandsFreeCookingActivity;
 import com.google.android.material.navigation.NavigationView;
 import android.view.Window;
 import androidx.core.content.ContextCompat;
@@ -92,6 +94,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             startActivity(new Intent(this, PantryActivity.class));
         } else if (id == R.id.nav_stats) {
             startActivity(new Intent(this, StatsActivity.class));
+        } else if (id == R.id.nav_handsfree) {
+            Intent intent = new Intent(MainActivity.this, HandsFreeCookingActivity.class);
+            startActivity(intent);
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
